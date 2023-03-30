948728b3b626fd402d76a28822a8ef4fee998cf2
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-03-30 12:00:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
+from googletrans import Translator
+from indictrans import Transliterator
 import openai
 
-# Initialize the OpenAI API key
-openai.api_key = "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"
+openai.api_key = ""
 
-# Define a function to handle the chatbot response
 def chatbot_response(prompt):
     completions = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=prompt,
         max_tokens=1024,
         n=1,
@@ -18,17 +18,32 @@ def chatbot_response(prompt):
     message = completions.choices[0].text
     return message
 
-# Set the title of the Streamlit app
-st.title("Mentor AI Chatbot by Bravish")
+def run_chatbot():
+    user_input = st.text_input("Enter your query in Hinglish:")
 
-# Create a text input field for the user to input their message
-user_input = st.text_input("You: ")
+    if user_input:
+        try:
+            # Convert Hinglish to Hindi
+            hindi_text = Transliterator(source='eng', target='hin').transform(user_input)
 
-# When the user clicks the "Submit" button, generate a response from the chatbot
-if st.button("Submit"):
-    if user_input.lower() == "try again":
-        st.write("Chatbot: Terminating the chatbot...")
-    else:
-        response = chatbot_response(user_input)
-        st.write("Chatbot: ", response)
+            # Convert Hindi to English
+            english_text = Translator().translate(hindi_text, dest='en').text
 
+            # Ask the OpenAI API
+            response = chatbot_response(english_text)
+
+            # Convert OpenAI API response to Hindi
+            hindi_response = Translator().translate(response, dest='hi').text
+
+            # Convert Hindi response to Hinglish
+            hinglish_response = Transliterator(source='hin', target='eng').transform(hindi_response)
+
+            # Output Hinglish response
+            st.success(f"Chatbot (Hinglish): {hinglish_response}")
+        except Exception as e:
+            st.error("Error: " + str(e))
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="Hinglish Chatbot")
+    st.title("Hinglish Chatbot")
+    run_chatbot()
