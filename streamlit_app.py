bd64299686c8b35dbf49418fd0005c1d5f1de56c
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-04-03 20:50:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import openai
 import speech_recognition as sr
 from gtts import gTTS
 import tempfile
-import streamlit_webrtc as webrtc
+from streamlit_webrtc import webrtc_streamer
 
 openai.api_key = st.secrets["openai_api_key"]
 
@@ -23,61 +23,56 @@ def chatbot_response(prompt):
     return message
 
 def run_chatbot():
-    try:
-        # Get user audio input
-        st.write("Speak your query:")
-        audio_input = webrtc.Streamer(
-            audio=True, 
-            key="audio-input",
-            constraints={"audio": True},
-        )
-        audio_input.start()
-        user_audio = st.video(
-            audio_input,
-            format="audio",
-            start_time=0,
-        )
+    # Get user audio input
+    st.write("Speak your query:")
+    audio_input = webrtc_streamer(
+        key="audio-input",
+        audio=True,
+        desired_output_format=webrtc_streamer.OutputFormat.AUDIO,
+        height=0,
+        width=0,
+        streaming_time_limit=7000,  # 7000 milliseconds
+        throttling_refresh_rate=1,
+    )
+    user_audio = st.audio(audio_input, format="audio/wav")
 
-        # Convert user audio input to Hindi text
-        st.write("Converting audio to text...")
-        recognizer = sr.Recognizer()
-        with sr.AudioFile(user_audio) as source:
-            audio_text = recognizer.recognize_google(source, language='hi-IN')
-        hindi_text = audio_text
+    # Convert user audio input to Hindi text
+    st.write("Converting audio to text...")
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(user_audio) as source:
+        audio_text = recognizer.recognize_google(source, language='hi-IN')
+    hindi_text = audio_text
 
-        # Convert Hindi to English
-        st.write("Converting Hindi text to English...")
-        english_text = Translator().translate(hindi_text, dest='en').text
+    # Convert Hindi to English
+    st.write("Converting Hindi text to English...")
+    english_text = Translator().translate(hindi_text, dest='en').text
 
-        # Ask the OpenAI API
-        st.write("Asking the OpenAI API...")
-        response = chatbot_response(english_text)
+    # Ask the OpenAI API
+    st.write("Asking the OpenAI API...")
+    response = chatbot_response(english_text)
 
-        # Convert OpenAI API response to Hindi
-        st.write("Converting OpenAI API response to Hindi...")
-        hindi_response = Translator().translate(response, dest='hi').text
+    # Convert OpenAI API response to Hindi
+    st.write("Converting OpenAI API response to Hindi...")
+    hindi_response = Translator().translate(response, dest='hi').text
 
-        # Output Hindi response
-        st.success(f"Chatbot (Hindi): {hindi_response}")
+    # Output Hindi response
+    st.success(f"Chatbot (Hindi): {hindi_response}")
 
-        # Convert Hindi response to Hinglish
-        st.write("Converting Hindi response to Hinglish...")
-        hinglish_response = Transliterator(source='hin', target='eng').transform(hindi_response)
+    # Convert Hindi response to Hinglish
+    st.write("Converting Hindi response to Hinglish...")
+    hinglish_response = Transliterator(source='hin', target='eng').transform(hindi_response)
 
-        # Convert Hinglish response to audio
-        st.write("Converting text to audio...")
-        tts = gTTS(hindi_response, lang='hi')
-        with tempfile.NamedTemporaryFile(delete=False) as tf:
-            tts.write_to_fp(tf)
-            tf.flush()
-            audio_file = tf.name
+    # Convert Hinglish response to audio
+    st.write("Converting text to audio...")
+    tts = gTTS(hindi_response, lang='hi')
+    with tempfile.NamedTemporaryFile(delete=False) as tf:
+        tts.write_to_fp(tf)
+        tf.flush()
+        audio_file = tf.name
 
-        # Output chatbot audio response
-        st.write("Chatbot Response:")
-        st.audio(audio_file)
-
-    except Exception as e:
-        st.error(f"Error occurred: {e}")
+    # Output chatbot audio response
+    st.write("Chatbot Response:")
+    st.audio(audio_file)
 
 if __name__ == "__main__":
     st.set_page_config(page_title="Hindi Chatbot")
