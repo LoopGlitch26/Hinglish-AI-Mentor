c19c28e6627fd23151ac76616d2258cd73836c83
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-04-06 22:20:45 -0700

    Update streamlit_app_vc.py

--- streamlit_app_vc.py
+++ streamlit_app_vc.py
@@ -1,4 +1,3 @@
-"""
 import streamlit as st
 import openai
 
@@ -19,6 +18,51 @@ def chatbot_response(prompt):
     message = completions.choices[0].text
     return message
 
+# Define the Streamlit app
+def main():
+    st.title("Hinglish Chatbot")
+        
+    # Prompt the user to select their business type
+    st.write("Please select your business type:")
+    business_type_options = ("Kirana shop", "Mobile repair shop", "Beauty salon", "Food truck", "Others")
+    business_type = st.selectbox("", business_type_options)
+
+    if business_type == "Others":
+        # Prompt the user to input their business type
+        business_type = st.text_input("Please enter your business type:")
+
+    # Prompt the user to input their business details in Hinglish
+    st.write("Please provide some details about your business in Hinglish:")
+    business_name = st.text_input("Business name")
+    business_description = st.text_area("Business description")
+    target_customers = st.text_area("Target customers")
+
+    # Use OpenAI to act as a mentor based on the provided details
+    st.write("Welcome to the mentorship program for your business, " + business_name + "!")
+    st.write("Here are some tips and advice based on the information you provided:")
+
+    # Ask the user if they would like to continue
+    st.write("Kya aap jaari rakhna chahte hain?")
+    ready = st.radio("", ("Haan", "Nahi"))
+
+    # If the user is ready, start the mentorship session
+    if ready == "Haan":
+        # Ask the user a question in Hinglish and get the mentor's response in Hinglish
+        question = st.text_input("Aap kya puchna chahte hain?")
+        prompt = "Hinglish mein jawaab dein: " + business_description + " " + target_customers + " " + question
+        response = chatbot_response(prompt)
+
+        # Display the mentor's response in Hinglish
+        st.write("Mentor: " + response)
+
+        # Allow the user to respond to the mentor's advice
+        while True:
+            user_input = st.text_input("Aap kya kehna chahte hain?")
+            if user_input:
+                response = chatbot_response(user_input)
+                st.write("Mentor: " + response)
+
+"""
 # Main function
 def main():
     st.title("Hinglish Chatbot")
@@ -32,10 +76,11 @@ def main():
         prompt = openai_prompt + "\nYou: " + user_input + "\nChatbot:"
         response = chatbot_response(prompt)
         st.write(response)
+"""
 
 if __name__ == "__main__":
     main()
-"""
+
     
     
     
@@ -43,7 +88,7 @@ if __name__ == "__main__":
     
     
     
-    
+"""    
 import streamlit as st
 from googletrans import Translator
 from indictrans import Transliterator
@@ -106,4 +151,4 @@ if __name__ == "__main__":
     st.set_page_config(page_title="Hinglish Chatbot")
     st.title("Hinglish Chatbot")
     run_chatbot()
-    
+"""    
