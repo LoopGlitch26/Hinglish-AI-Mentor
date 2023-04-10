246db2b49d02ab7089708d1d41daca1dec386250
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-04-09 23:20:37 -0700

    Update TTS

--- streamlit_app_vc.py
+++ streamlit_app_vc.py
@@ -2,14 +2,10 @@ import streamlit as st
 from googletrans import Translator
 from indictrans import Transliterator
 import openai
-import speech_recognition as sr
-import pyttsx3
+from gtts import gTTS
+from io import BytesIO
 
 openai.api_key = st.secrets["openai_api_key"]
-translator = Translator()
-transliterator = Transliterator(source='eng', target='hin')
-r = sr.Recognizer()
-engine = pyttsx3.init()
 
 def chatbot_response(prompt):
     completions = openai.Completion.create(
@@ -23,37 +19,29 @@ def chatbot_response(prompt):
     message = completions.choices[0].text
     return message
 
-def recognize_speech():
-    with sr.Microphone() as source:
-        st.write("Say something!")
-        audio = r.listen(source)
-        try:
-            text = r.recognize_google(audio)
-            return text
-        except sr.UnknownValueError:
-            return "I'm sorry, I didn't understand that."
-        except sr.RequestError:
-            return "Sorry, my speech recognition service is down at the moment."
-
-def generate_response(text):
-    try:
-        hindi_text = transliterator.transform(text)
-        english_text = translator.translate(hindi_text, dest='en').text
-        prompt = "Answer in details in Hinglish language. Aap ek Microentreprenuer ke Mentor hai. Microentreprenuer ka sawaal:\nYou: " + english_text
-        response = chatbot_response(prompt)
-        st.success(f"Chatbot: {response}")
-        engine.say(response)
-        engine.runAndWait()
-    except Exception as e:
-        st.error("Error: " + str(e))
+def text_to_speech(text):
+    audio_bytes = BytesIO()
+    tts = gTTS(text=text, lang="en")
+    tts.write_to_fp(audio_bytes)
+    audio_bytes.seek(0)
+    return audio_bytes.read()
 
 def run_chatbot():    
-    st.write("Click the microphone button and speak in Hindi or Hinglish!")
-    if st.button("Start Recording"):
-        text = recognize_speech()
-        generate_response(text)
+    default_prompt = "Answer in details in Hinglish language. Aap ek Microentreprenuer ke Mentor hai. Microentreprenuer ka sawaal:"
+    user_input = st.text_input("Enter your query in Hinglish:")
+
+    if user_input:
+        try:
+            hindi_text = Transliterator(source='eng', target='hin').transform(user_input)
+            english_text = Translator().translate(hindi_text, dest='en').text
+            prompt = default_prompt + "\nYou: " + english_text      
+            response = chatbot_response(prompt)
+            st.success(f"Chatbot: {response}")
+            st.audio(text_to_speech(response), format="audio/wav")
+        except Exception as e:
+            st.error("Error: " + str(e))
 
 if __name__ == "__main__":
     st.set_page_config(page_title="Hinglish Chatbot")
     st.title("Hinglish Chatbot")
-    run_chatbot()
+    run_chatbot() 
