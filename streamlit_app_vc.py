b1408d05d712ce725251b51e19756b513a2e3da4
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-04-09 22:08:01 -0700

    Update streamlit_app_vc.py

--- streamlit_app_vc.py
+++ streamlit_app_vc.py
@@ -12,7 +12,7 @@ def chatbot_response(prompt):
         max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.8,
+        temperature=0.5,
     )
     message = completions.choices[0].text
     return message    
