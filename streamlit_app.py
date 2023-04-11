f0368d1410ae5d69d49cc2b09d8dfb71c6e43418
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-04-10 20:32:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,16 @@
 import streamlit as st
-from googletrans import Translator
-from indictrans import Transliterator
 import openai
-import time
-import io
-import whisper
 from gtts import gTTS
+from io import BytesIO
+import scipy.io.wavfile
 import wavio
+import whisper
+import os
+from dotenv import load_dotenv
 
-openai.api_key = st.secrets["openai_api_key"]
+load_dotenv()
 
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def chatbot_response(prompt):
     completions = openai.Completion.create(
@@ -23,76 +24,61 @@ def chatbot_response(prompt):
     message = completions.choices[0].text
     return message
 
-
-def text_to_speech(text):
-    audio_bytes = io.BytesIO()
-    tts = gTTS(text=text, lang="en")
-    tts.save(audio_bytes)
+def text_to_speech(text, speed=1.5):
+    audio_bytes = BytesIO()
+    tts = gTTS(text=text, lang="hi")
+    tts_speed = str(speed)
+    tts.save("temp.mp3")
+    wavio.write("temp.wav", scipy.io.wavfile.read("temp.mp3")[0], scipy.io.wavfile.read("temp.mp3")[1])
+    with open("temp.wav", "rb") as f:
+        audio_bytes.write(f.read())
     audio_bytes.seek(0)
+    os.remove("temp.mp3")
+    os.remove("temp.wav")
     return audio_bytes.read()
 
+def record_audio():
+    return st.audio_recorder("recording.wav", format="wav")
 
-def record_audio(duration):
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        st.write("Please start speaking...")
-        audio = r.record(source, duration=duration)
-        st.write("Recording completed...")
-    return audio
-
-
-def speech_to_text(audio):
-    r = sr.Recognizer()
-    try:
-        text = r.recognize_google(audio)
-        return text
-    except sr.UnknownValueError:
-        st.write("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        st.write("Could not request results from Google Speech Recognition service; {0}".format(e))
-
+def process_audio(audio_data):
+    scipy.io.wavfile.write("recording.wav", 44100, audio_data)
+    text = whisper.transcribe("recording.wav")
+    os.remove("recording.wav")
+    return text
 
 def run_chatbot():
-    default_prompt = "Answer in details in Hinglish language. Aap ek Microentreprenuer ke Mentor hai. Microentreprenuer ka sawaal: "
-    inp = st.selectbox("Which input form would you like", ["Text", "Voice"])
+    input_mode = st.selectbox("Select Input Mode:", ["Text", "Voice"])
 
-    if inp == "Text":
+    if input_mode == "Text":
+        default_prompt = "Answer in details in Hinglish language. Aap ek Microentreprenuer ke Mentor hai. Microentreprenuer ka sawaal:"
         user_input = st.text_input("Enter your query in Hinglish:")
+
         if user_input:
             try:
-                hindi_text = Transliterator(source="eng", target="hin").transform(user_input)
-                english_text = Translator().translate(hindi_text, dest="en").text
-                prompt = default_prompt + "\nYou: " + english_text
+                prompt = default_prompt + "\nYou: " + user_input
                 response = chatbot_response(prompt)
                 st.success(f"Chatbot: {response}")
-                st.audio(text_to_speech(response), format="audio/mp3")
+                st.audio(text_to_speech(response), format="audio/wav")
             except Exception as e:
                 st.error("Error: " + str(e))
 
-    else:
-        rec = st.button("Record the question")
-        if rec:
-            duration = 10
-            audio = record_audio(duration)
-            text = speech_to_text(audio)
-            if text:
-                st.success(f"Recorded text: {text}")
+    elif input_mode == "Voice":
+        st.warning("Please speak your query in Hinglish:")
+        audio_data = record_audio()
 
-        submit = st.button("Generate the answer")
-        if submit:
+        if audio_data:
             try:
-                prompt = "Answer in details in Hinglish language. Aap ek Microentreprenuer ke Mentor hai. Microentreprenuer ka sawaal: " + text + " ?"
+                text = process_audio(audio_data)
+                hindi_text = Transliterator(source='eng', target='hin').transform(text)
+                english_text = Translator().translate(hindi_text, dest='en').text
+                prompt = "Answer in details in Hinglish language. Aap ek Microentreprenuer ke Mentor hai. Microentreprenuer ka sawaal:\nYou: " + english_text
                 response = chatbot_response(prompt)
                 st.success(f"Chatbot: {response}")
-                audio_bytes = text_to_speech(response)
-                with io.BytesIO(audio_bytes) as stream:
-                    with wavio.open(stream, mode="rb") as wav_file:
-                        wav_data = wav_file.data
-                        whisper.save_model(wav_data, "model")
-                st.audio(audio_bytes, format="audio/mp3")
+                st.audio(text_to_speech(response), format="audio/wav")
             except Exception as e:
                 st.error("Error: " + str(e))
-
 
 if __name__ == "__main__":
+    st.set_page_config(page_title="Hinglish Chatbot")
+    st.title("Hinglish Chatbot")
     run_chatbot()
