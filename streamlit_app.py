c1fb3faf4d634076a70cbbdff07e7dee0d870c9f
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-04-11 00:55:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,8 @@ def main():
     openai.api_key = st.secrets["openai_api_key"]
     title='<p style="font-family:Algerian; color:Cyan; align:center; font-size: 42px;">Hinglish AI ChatBot<p>'
     st.markdown(title,unsafe_allow_html=True)
-    footer = '<p style=\'text-align: center; font-size: 0.8em;\'>Copyright © 2023 My Company</p>'
-    st.markdown(footer, unsafe_allow_html=True)
-    st.markdown("Hi, I'm MicroMentor, powered by AI.\nI'm here to help you with your micro-entrepreneurial issues.\nI can provide you with business tips and advice based on your business concerns.\nI can listen to you and I can also read your queries.\nSo, let's get started.")
+    
+    st.markdown("AI-powered chatbot to assist you with your business queries and provide you with relevant advice.")
     inp=st.selectbox("Which input form would you like", ['Text', 'Voice'])
     
     form = st.form(key="user_settings")
@@ -83,7 +82,10 @@ def main():
                 st.markdown(f"Here's some advice for your query:")
                 st.write(res)
             except Exception as e:
-                st.error("Error: " + str(e))            
+                st.error("Error: " + str(e))        
+    
+    footer = '<p style=\'text-align: center; font-size: 0.8em;\'>Copyright © 2023 LoopGlitch26</p>'
+    st.markdown(footer, unsafe_allow_html=True)
         
 if __name__ == "__main__":
     st.set_page_config(page_title="Hinglish Chatbot")
