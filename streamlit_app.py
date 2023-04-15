8c13215d62df182627831e4c69fd6cae7ea5dcde
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-04-15 13:00:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import os
 import openai
 from googletrans import Translator
 from indictrans import Transliterator
+import time
 from io import BytesIO
 from gtts import gTTS
+from streamlit.web import cli as stcli
+from streamlit import runtime
 import streamlit as st
 from dotenv import load_dotenv
 import streamlit.components.v1 as components
@@ -13,20 +16,18 @@ import wavio as wv
 import whisper
 import numpy as np
 import soundfile as sf
-from streamlit import runtime
 
 def main():
-    load_dotenv()
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    title = '<p style="color:Red; text-align:center; font-size: 42px;">Hinglish ChatBot</p>'
-    st.markdown(title, unsafe_allow_html=True)
+    openai.api_key = st.secrets["openai_api_key"]
+    title='<p style="color:Red; align:center; font-size: 42px;">Hinglish ChatBot<p>'
+    st.markdown(title,unsafe_allow_html=True)
     
     st.markdown("AI-powered chatbot to assist you with your business queries and provide you with relevant advice.")
-    inp = st.selectbox("Which input form would you like", ['Text', 'Voice'])
+    inp=st.selectbox("Which input form would you like", ['Text', 'Voice'])
     
     default_prompt = "Answer in details in Hinglish language. Aap ek Microentreprenuer ke Mentor hai. Microentreprenuer ka sawaal: "
     form = st.form(key="user_settings")
-    if inp == "Text":
+    if inp=="Text":
         with form:
             kw = st.text_input("Enter your query in Hinglish:", key="en_keyword", placeholder="Type here...")
             submit = form.form_submit_button("Get advice")
@@ -38,48 +39,38 @@ def main():
                         engine="text-davinci-003", 
                         prompt=default_prompt + "\n" + english_text,
                         max_tokens=1024,
-                        n=1,
+                        n = 1,
                         stop=None,
                         temperature=0.8,
                     )
-                    res = response.choices[0].text
-                    myobj = gTTS(text=res, lang='hi', slow=False)
-                    mp3_play = BytesIO()
+                    res=response.choices[0].text
+                    myobj = gTTS(text=res,lang='hi', slow=False)
+                    mp3_play=BytesIO()
                     myobj.write_to_fp(mp3_play)
-                    st.audio(mp3_play, format="audio/mp3", start_time=0)
+                    st.audio(mp3_play,format="audio/mp3", start_time=0)
                     st.success(res)
                 except Exception as e:
                     st.error("Error: " + str(e))
-      
+                
     else:
-        rec = st.button("Record your query")
-        st.markdown("Please don't use the stop button, it terminates the process abruptly. Wait for the 'get advice' button to appear and click it")
-        text = ""
-        if rec:
-            st.warning("Speak your query after clicking the button below")
-            audio_bytes = st.record(key="audio")
+        rec = st_audio_recorder(sample_rate=16000, duration=10, key="recorder")
+        st.markdown("Click on the record button and speak your query...")
+        if st.button("Record"):
             try:
-                from google.cloud import speech_v1
-                from google.cloud.speech_v1 import enums
-                from google.cloud.speech_v1 import types
-                client = speech_v1.SpeechClient()
-                audio = types.RecognitionAudio(content=audio_bytes.getvalue())
-                config = types.RecognitionConfig(
-                    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-                    sample_rate_hertz=16000,
-                    language_code="hi-IN",
-                )
-                response = client.recognize(config=config, audio=audio)
-                text = response.results[0].alternatives[0].transcript
-                st.success("Query recorded successfully")
-            except Exception as e:
-                st.error("An error occurred while processing your query: {}".format(str(e)))
- 
-      
-        submit = st.button("Get advice")
-        if submit:
-            try:
-                english_text = Translator().translate(text, dest='en').text
+                with rec:
+                    audio = rec.record(duration=10)
+                st.success("Recording complete!")
+                write("audio.wav", 16000, audio)
+                audio_data, sr = sf.read("audio.wav")
+                processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-xlsr-53")
+                model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-xlsr-53")
+                input_values = processor(audio_data, sampling_rate=sr, return_tensors="pt").input_values
+                with torch.no_grad():
+                    logits = model(input_values).logits
+                predicted_ids = torch.argmax(logits, dim=-1)
+                transcriptions = processor.batch_decode(predicted_ids)
+                hindi_text = Transliterator(source='eng', target='hin').transform(transcriptions[0])
+                english_text = Translator().translate(hindi_text, dest='en').text
                 response = openai.Completion.create(
                     engine="text-davinci-003", 
                     prompt=default_prompt + "\n" + english_text,
@@ -88,14 +79,11 @@ def main():
                     stop=None,
                     temperature=0.8,
                 )
-                res=response.choices[0].text
-                myobj = gTTS(text=res,lang='hi', slow=False)
-                mp3_play=BytesIO()
-                myobj.write_to_fp(mp3_play)
-                st.audio(mp3_play,format="audio/mp3", start_time=0)
-                st.success(res)
+                res = response.choices[0].text
+                myobj = gTTS(text=res, lang='hi', slow=False)
+                mp3_play = BytesIO()
             except Exception as e:
-                st.error("Error: " + str(e))       
+                st.error(f"Error: {e}")
                                     
     footer = '<p style=\'text-align: center; font-size: 0.8em;\'>Copyright © Bravish</p>'
     st.markdown(footer, unsafe_allow_html=True)        
