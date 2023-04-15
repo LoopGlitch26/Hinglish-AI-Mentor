be61e5e140b302df0c64dd8ea1e32f9156dcada9
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-04-15 13:21:28 -0700

    Update streamlit_app_vc.py

--- streamlit_app_vc.py
+++ streamlit_app_vc.py
@@ -2,9 +2,9 @@ import streamlit as st
 from googletrans import Translator
 from indictrans import Transliterator
 import openai
-from gtts import gTTS
+from google.cloud import speech_v1
+from google.cloud.speech_v1 import enums
 from io import BytesIO
-import speech_recognition as sr
 
 openai.api_key = st.secrets["openai_api_key"]
 
@@ -30,24 +30,32 @@ def text_to_speech(text):
 def run_chatbot():    
     default_prompt = "Answer in details in Hinglish language. Aap ek Microentreprenuer ke Mentor hai. Microentreprenuer ka sawaal:"
     
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        st.markdown("Click on the record button and speak your query...")
-        audio = r.listen(source)
-    st.success("Recording complete!")
+    st.write("Speak now:")
+    audio_bytes = audio_recorder()
     
-    try:
-        text = r.recognize_google(audio, language='hi-IN')
-        hindi_text = Transliterator(source='eng', target='hin').transform(text)
-        english_text = Translator().translate(hindi_text, dest='en').text
-        prompt = default_prompt + "\nYou: " + english_text      
-        response = chatbot_response(prompt)
-        st.success(f"Chatbot: {response}")
-        st.audio(text_to_speech(response), format="audio/wav")
-    except sr.UnknownValueError:
-        st.warning("Could not understand audio")
-    except sr.RequestError as e:
-        st.error(f"Error: {e}")
+    if audio_bytes:
+        try:
+            client = speech_v1.SpeechClient()
+            audio = speech_v1.RecognitionAudio(content=audio_bytes)
+            config = speech_v1.RecognitionConfig(
+                encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+                sample_rate_hertz=16000,
+                language_code="hi-IN",
+            )
+            response = client.recognize(config=config, audio=audio)
+            user_input = response.results[0].alternatives[0].transcript
+            st.write("You said:", user_input)
+
+            hindi_text = Transliterator(source='eng', target='hin').transform(user_input)
+            english_text = Translator().translate(hindi_text, dest='en').text
+            prompt = default_prompt + "\nYou: " + english_text      
+            response = chatbot_response(prompt)
+            st.success(f"Chatbot: {response}")
+            st.audio(text_to_speech(response), format="audio/wav")
+        except Exception as e:
+            st.error("Error: " + str(e))
+    else:
+        st.warning("No audio input detected. Please try again.")
 
 if __name__ == "__main__":
     st.set_page_config(page_title="Hinglish Chatbot")
