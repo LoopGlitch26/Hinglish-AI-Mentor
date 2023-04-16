1f21098b833f69b4aaa7c9a58b5c7de9d344f51f
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-04-15 20:45:45 -0700

    revtb

--- streamlit_app_vc.py
+++ streamlit_app_vc.py
@@ -2,8 +2,7 @@ import streamlit as st
 from googletrans import Translator
 from indictrans import Transliterator
 import openai
-from google.cloud import speech_v1
-from google.cloud.speech_v1 import enums
+from gtts import gTTS
 from io import BytesIO
 
 openai.api_key = st.secrets["openai_api_key"]
@@ -29,23 +28,10 @@ def text_to_speech(text):
 
 def run_chatbot():    
     default_prompt = "Answer in details in Hinglish language. Aap ek Microentreprenuer ke Mentor hai. Microentreprenuer ka sawaal:"
-    
-    st.write("Speak now:")
-    audio_bytes = audio_recorder()
-    
-    if audio_bytes:
-        try:
-            client = speech_v1.SpeechClient()
-            audio = speech_v1.RecognitionAudio(content=audio_bytes)
-            config = speech_v1.RecognitionConfig(
-                encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-                sample_rate_hertz=16000,
-                language_code="hi-IN",
-            )
-            response = client.recognize(config=config, audio=audio)
-            user_input = response.results[0].alternatives[0].transcript
-            st.write("You said:", user_input)
+    user_input = st.text_input("Enter your query in Hinglish:")
 
+    if user_input:
+        try:
             hindi_text = Transliterator(source='eng', target='hin').transform(user_input)
             english_text = Translator().translate(hindi_text, dest='en').text
             prompt = default_prompt + "\nYou: " + english_text      
@@ -54,8 +40,6 @@ def run_chatbot():
             st.audio(text_to_speech(response), format="audio/wav")
         except Exception as e:
             st.error("Error: " + str(e))
-    else:
-        st.warning("No audio input detected. Please try again.")
 
 if __name__ == "__main__":
     st.set_page_config(page_title="Hinglish Chatbot")
