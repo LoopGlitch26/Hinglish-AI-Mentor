5f0e23e09f6db4dc2f05730849fbe697d08d1455
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-04-15 20:55:58 -0700

    Update streamlit_app_vc.py

--- streamlit_app_vc.py
+++ streamlit_app_vc.py
@@ -4,6 +4,9 @@ from indictrans import Transliterator
 import openai
 from gtts import gTTS
 from io import BytesIO
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
 openai.api_key = st.secrets["openai_api_key"]
 
@@ -29,6 +32,40 @@ def text_to_speech(text):
 def run_chatbot():    
     default_prompt = "Answer in details in Hinglish language. Aap ek Microentreprenuer ke Mentor hai. Microentreprenuer ka sawaal:"
     user_input = st.text_input("Enter your query in Hinglish:")
+    stt_button = Button(label="Speak", width=100)
+    
+    stt_button.js_on_event("button_click", CustomJS(code="""
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+
+        recognition.onresult = function (e) {
+            var value = "";
+            for (var i = e.resultIndex; i < e.results.length; ++i) {
+                if (e.results[i].isFinal) {
+                    value += e.results[i][0].transcript;
+                }
+            }
+            if ( value != "") {
+                document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+            }
+        }
+        recognition.start();
+    """))
+    
+    result = streamlit_bokeh_events(
+        stt_button,
+        events="GET_TEXT",
+        key="listen",
+        refresh_on_update=False,
+        override_height=75,
+        debounce_time=0
+    )
+
+    if result:
+        if "GET_TEXT" in result:
+            user_input = result.get("GET_TEXT")
+            st.text_input("Your query is:", user_input)
 
     if user_input:
         try:
