aa7af89ac13601faafe51e62b7af148b78e2fb9d
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-04-16 09:55:58 -0700

    Update streamlit_app_vc.py

--- streamlit_app_vc.py
+++ streamlit_app_vc.py
@@ -21,7 +21,7 @@ def chatbot_response(prompt):
 
 def text_to_speech(text):
     audio_bytes = BytesIO()
-    tts = gTTS(text=text, lang="hi", slow=True)
+    tts = gTTS(text=text, lang="hi", slow=False)
     tts.write_to_fp(audio_bytes)
     audio_bytes.seek(0)
     return audio_bytes.read()
