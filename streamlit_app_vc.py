37eab87beb29777288c106b14b751d86f2fa71ec
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-05-27 10:26:29 -0700

    Update streamlit_app_vc.py

--- streamlit_app_vc.py
+++ streamlit_app_vc.py
@@ -4,6 +4,7 @@ from indictrans import Transliterator
 import openai
 from gtts import gTTS
 from io import BytesIO
+import assemblyai
 
 openai.api_key = st.secrets["openai_api_key"]
 
@@ -29,6 +30,7 @@ def text_to_speech(text):
 def run_chatbot():    
     default_prompt = "Answer in details in Hinglish language. Aap ek Microentreprenuer ke Mentor hai. Microentreprenuer ka sawaal:"
     user_input = st.text_input("Enter your query in Hinglish:")
+    user_audio = st.file_uploader("Or upload an audio file:", type=["wav", "mp3"])
 
     if user_input:
         try:
@@ -41,7 +43,24 @@ def run_chatbot():
         except Exception as e:
             st.error("Error: " + str(e))
 
+    if user_audio:
+        try:
+            assemblyai.settings.api_key = "0b0a5dff3d4a4893af85204dc660f88b"
+            transcriber = assemblyai.Transcriber()
+            audio_data = user_audio.read()
+            transcript = transcriber.transcribe(audio_data)
+            transcribed_text = transcript.text
+
+            hindi_text = Transliterator(source='eng', target='hin').transform(transcribed_text)
+            english_text = Translator().translate(hindi_text, dest='en').text
+            prompt = default_prompt + "\nYou: " + english_text      
+            response = chatbot_response(prompt)
+            st.success(f"Chatbot: {response}")
+            st.audio(text_to_speech(response), format="audio/wav")
+        except Exception as e:
+            st.error("Error: " + str(e))
+
 if __name__ == "__main__":
     st.set_page_config(page_title="Hinglish Chatbot")
     st.title("Hinglish Chatbot")
-    run_chatbot() 
+    run_chatbot()
